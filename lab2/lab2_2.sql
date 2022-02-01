6fdbec59d77aa69a0ed5aa0169901c74a0718ff7
Max <ma8251so-s@student.lu.se>
2022-02-01 13:54:48 +0100

    Max & marcus är done, get rekt freddan

--- lab2/lab2_2.sql
+++ lab2/lab2_2.sql
@@ -1,3 +1,6 @@
+PRAGMA foreign_keys=OFF;
+
+DROP TABLE IF EXISTS theater;
 DROP TABLE IF EXISTS chains;
 CREATE TABLE chains (
     c_name          TEXT,
@@ -13,15 +16,16 @@ CREATE TABLE theaters (
 
 DROP TABLE IF EXISTS screenings;
 CREATE TABLE screenings (
-    screening_id    INT,
+    screening_id    INTEGER,
     start_time      TIME,
     start_date      DATE,
+    free_seats      INT,
     t_name          TEXT,
     m_title         TEXT,
     production_year INT,
     FOREIGN KEY     (t_name) REFERENCES theaters(t_name),
     FOREIGN KEY     (m_title, production_year) REFERENCES movies(m_title, production_year)
-    PRIMARY KEY     (start_time, t_name, m_title)
+    PRIMARY KEY     (screening_id)
 );
 
 DROP TABLE IF EXISTS movies;
@@ -73,30 +77,30 @@ VALUES  ('The Lord of the Rings: The Fellowship of the Ring', 2001, 178, 'tt0120
         ('Monty Python and the Holy Grail', 1975, 91, 'tt0071853');
 
 INSERT
-INTO    screenings(start_time, start_date, t_name, m_title, production_year)
-VALUES  ('2022-02-01', '19:30', 'Filmstaden Lund', 'Demon Slayer: Mugen Train', 2020),
-        ('2022-02-01', '20.30', 'Royal', 'Snatch', 2000),
-        ('2022-02-01', '20.30', 'Storgatan', 'Monty Python and the Holy Grail', 1975),
-        ('2022-02-01', '20.30', 'Entre', 'Batman Begins', 2005);
+INTO    screenings(start_date, start_time, t_name, free_seats, m_title, production_year)
+VALUES  ('2022-02-01', '19:30', 'Filmstaden Lund', 100, 'Demon Slayer: Mugen Train', 2020),
+        ('2022-02-01', '20:30', 'Royal', 500, 'Snatch', 2000),
+        ('2022-02-01', '20:30', 'Storgatan', 300, 'Monty Python and the Holy Grail', 1975),
+        ('2022-02-01', '20:30', 'Entre', 500, 'Batman Begins', 2005);
 
 INSERT
-INTO tickets(screening_id, username)
-VALUES(1, 'marcusoft'),
-      (1, 'axley'),
-      (2, 'marcusoft'),
-      (2, 'stifi'),
-      (3, 'Greven'),
-      (3, 'axley'),
-      (3, 'sollerito'),
-      (3, 'MrFiskpinnar'),
-      (4, 'marcusoft'),
-      (4, 'Sollerito');
+INTO    tickets(screening_id, username)
+VALUES  (1, 'marcusoft'),
+        (1, 'axley'),
+        (2, 'marcusoft'),
+        (2, 'stifi'),
+        (3, 'Greven'),
+        (3, 'axley'),
+        (3, 'sollerito'),
+        (3, 'MrFiskpinnar'),
+        (4, 'marcusoft'),
+        (4, 'Sollerito');
 
 INSERT
-INTO customers(username, full_name, pwd, c_name)
-VALUES ('marcusoft', 'marcusbegic','kingen123', 'Filmstaden'),
-       ('stifi', 'dylanfrost','somethingstrange','Filmstaden'),
-       ('axley', 'axelbengtsson','mari6969','Filmstaden'),
-       ('Greven', 'FredrikHornDannertAfAminne','pleb','Filmstaden'),
-       ('Sollerito', 'MaxSoller','bayern','Filmstaden'),
-       ('MrFiskpinnar', 'HenkeRasmusson','obiOneknobe','Filmstaden');
+INTO    customers(username, full_name, pwd, c_name)
+VALUES  ('marcusoft', 'marcusbegic','kingen123', 'Filmstaden'),
+        ('stifi', 'dylanfrost','somethingstrange','Filmstaden'),
+        ('axley', 'axelbengtsson','mari6969','Filmstaden'),
+        ('Greven', 'FredrikHornDannertAfAminne','pleb','Filmstaden'),
+        ('Sollerito', 'MaxSoller','bayern','Filmstaden'),
+        ('MrFiskpinnar', 'HenkeRasmusson','obiOneknobe','Filmstaden');
