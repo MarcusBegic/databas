c74da5de939bbfb478adb208d566b1043243021f
Marcus Begic <marcus.begic.6373@student.lu.se>
2022-02-18 05:40:55 +0100

    almost done 3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -6,6 +6,7 @@ import hashlib
 db = sqlite3.connect('movies.sqlite')
 
 
+
 @get('/ping')
 def get_pong():
     response.status = 200
@@ -59,16 +60,19 @@ def post_users():
     try:
         user = request.json
         c = db.cursor()
+        print("here2")
         c.execute(
             """
             INSERT
-            INTO       customer(username, full_name, pwd)
+            INTO       customers(username, full_name, pwd)
             VALUES     (?, ?, ?)
             RETURNING  username
             """,
             [user['username'], user['fullName'], hash(user['pwd'])]
         )
+        print("here1")
         found = c.fetchone()
+        print("here3")
         if not found:
             response.status = 400
             return "Illegal..."
@@ -89,11 +93,11 @@ def post_movies():
         c.execute(
             """ 
             INSERT
-            INTO        movies (m_title, imdb_key, production_year)
+            INTO        movies(m_title, imdb_key, production_year)
             VALUES      (?, ?, ?)
             RETURNING   imdb_key
             """,
-            [movie['imdbKey'], movie['title'], movie['year']]
+            [movie['title'], movie['imdbKey'], movie['year']]
         )
         found = c.fetchone()
         if not found:
@@ -131,11 +135,153 @@ def post_performances():
             return "Illegal..."
         else:
             response.status = 201
-            performance_id, = found
+            screening_id, = found
             return f"/performances/{screening_id}\n"
     except:
         response.status = 400
         return "No such movie or theater"
+
+
+@get('/movies')
+def get_movies():
+    # -d '{"imdbKey": "tt0111161", "title": "The Shawshank Redemption", "year": "1994"}' -H "Content-Type: application/json"
+    query = """
+            SELECT   imdb_key, m_title, production_year
+            FROM     movies
+            WHERE    1=1
+            """
+    
+    params = []
+    if request.query.title:
+        query += " AND m_title = ?"
+        params.append(unquote(request.query.title))
+    if request.query.year:
+        query += " AND production_year = ?"
+        params.append(request.query.year)
+        
+    c = db.cursor()
+    c.execute(query, params)
+    found = [{"imdbKey": imdb_key, "title": m_title, "year": production_year}
+            for imdb_key, m_title, production_year in c]
+    response.status = 200
+    return {"data": found}
+
+
+@get('/movies/<imdb_key>')
+def get_specific_movie(imdb_key):
+    c = db.cursor()
+    c.execute(
+        """
+        SELECT   imdb_key, m_title, production_year
+        FROM     movies
+        WHERE    imdb_key = ?
+        """,
+        [imdb_key]
+    )
+    found = [{"imdbKey": imdb_key, "title": m_title, "year": production_year}
+             for imdb_key, m_title, production_year in c]
+    response.status = 200
+    return {"data": found}
+
+
+@get('/performances')
+def performances():
+    c = db.cursor()
+    c.execute(
+        """
+        WITH taken_seats AS (
+            SELECT t_name, count(ticket_id) AS tickets_bought
+            FROM screenings
+            LEFT OUTER JOIN tickets
+            USING (screening_id)
+            GROUP BY screening_id
+        )
+
+        SELECT   DISTINCT screening_id, start_date, start_time, m_title, production_year, t_name, (capacity - tickets_bought) AS available_seats
+        FROM     screenings
+        JOIN     movies
+        USING    (imdb_key)
+        JOIN     taken_seats
+        USING    (t_name)
+        JOIN     theaters
+        USING    (t_name)
+        """
+    )
+    found = [{"performanceId": screening_id, "date": start_date, "startTime": start_time, "title": m_title, "year": production_year, "theater": t_name, "remainingSeats": available_seats}
+            for screening_id, start_date, start_time, m_title, production_year, t_name, available_seats in c]
+    response.status = 200
+    return {"data": found}
+
+
+@post('/tickets')
+def post_tickets():
+    # -d '{"username": "daniel123", "pwd": "hej1234", "performanceId": 1}' -H "Content-Type: application/json"
+    try:
+        c = db.cursor()
+        
+        ticket = request.json
+        c.execute(
+            """
+            SELECT  username, pwd
+            FROM    customers
+            WHERE   username = ? AND pwd = ?
+            """,
+            [ticket['username'], hash(ticket['pwd'])]
+        )
+        found = c.fetchone()
+        if not found:
+            response.status = 401
+            return "Wrong user credentials"
+        
+        c.execute(
+            """
+            WITH remaining_seats AS (
+                SELECT t_name, count(ticket_id) AS tickets_bought
+                FROM screenings
+                LEFT OUTER JOIN tickets
+                USING (screening_id)
+                GROUP BY screening_id
+            )
+
+            SELECT  screening_id, (capacity - tickets_bought) AS available_seats
+            FROM    screenings
+            JOIN    remaining_seats
+            USING   (t_name)
+            JOIN    theaters
+            USING   (t_name)
+            WHERE   screening_id = ?
+            """,
+            [ticket['performanceId']]
+        )
+        remaining_seats = c.fetchone()[1]
+        if remaining_seats == 0:
+            response.status = 400
+            return "No tickets left"
+            
+        c.execute(
+            """     
+            INSERT
+            INTO        tickets (screening_id, username)
+            VALUES      (?, ?)
+            RETURNING   ticket_id
+            """,
+            [ticket['performanceId'], ticket['username']]
+        )
+        found = c.fetchone()
+        if not found:
+            response.status = 400
+            return "Error"
+        else:
+            response.status = 201
+            ticket_id = found
+            return f"/tickets/{ticket_id}\n"
+    except:
+        response.status = 400
+        return "Error"
+
+
+
+
 
 
 run(host='localhost', port=7007)
